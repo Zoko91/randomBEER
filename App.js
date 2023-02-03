e971dc100cd85c4a293a81c9248b840dc6da6ab9
Zoko91 <josephbeasse@gmail.com>
2023-02-03 11:03:08 +0100

    final version

--- App.js
+++ App.js
@@ -1,20 +1,122 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from "react";
+import {
+    ActivityIndicator,
+    Image,
+    StyleSheet,
+    Text,
+    View,
+    ScrollView,
+    FlatList,
+    Button,
+    TextInput,
+    Alert,
+} from "react-native";
+
+// const getRandomNumber = () => {
+//     return Math.floor(Math.random() * 100) + 1
+// };
+
+
+
+
+
+// const getRandomBEER = () => {
+//     return fetch('https://api.punkapi.com/v2/beers/random')
+//         .then(response => response.json())
+//         .then(json => {
+//             return json.name;
+//         })
+//         .catch(error => {
+//             console.error(error);
+//         });
+// };
+
+// class MyComponent extends React.Component {
+//     render() {
+//         return (
+//             <View style={styles.compo}>
+//                 <Text style={styles.title}>
+//                     Bienvenue sur
+//                 </Text>
+//                 <Text style={styles.title}>
+//                     RandomBEER !
+//                 </Text>
+//                 {TestComponent()}
+//                 <Text style={styles.texte}>
+//                     {/*{getRandomBEER().toString()}*/}
+//                     {/*{getRandomNumber().toString()}*/}
+//                 </Text>
+//             </View>
+//         );
+//     }
+// }
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    const [data, setData] = useState({});
+    const getBeer = async () => {
+        try {
+            const response = await fetch('https://api.punkapi.com/v2/beers/random');
+            const json = await response.json();
+            setData(json[0]);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    useEffect(() => {
+        getBeer();
+    }, []);
+
+    return (
+        // <MyComponent/>
+        <View style={styles.compo}>
+            <Text style={styles.title}>
+                Bienvenue sur
+            </Text>
+            <Text style={styles.title}>
+                RandomBEER !
+            </Text>
+            <Text style={styles.texte}>
+                {data.name}
+            </Text>
+            <Text style={styles.desc}>
+                {data.description}
+            </Text>
+            <Image
+                style={styles.img}
+                source={{
+                    uri: `${data.image_url}`,
+                }}
+            />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    texte: {
+        color: "black",
+        fontSize: 20,
+        marginTop:20,
+    },
+    title: {
+        color: "black",
+        fontSize: 30,
+        fontWeight: "bold",
+    },
+    compo: {
+        marginTop: 70,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    desc: {
+        fontSize:15,
+        margin:10,
+
+    },
+    img:{
+        height:200,
+        width:100,
+    }
 });
